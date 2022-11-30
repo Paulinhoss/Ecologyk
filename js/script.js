c9d660c8c841581181cb482d5dbac1611edb84f6
Paulinhoss <119386814+Paulinhoss@users.noreply.github.com>
2022-11-30 10:29:06 -0300

    Arquivos

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+
+window.sr = ScrollReveal({ reset:true })
+ScrollReveal().reveal('.about', {
+    duration:700
+   
+})
+ScrollReveal().reveal('.events', {
+    duration:700
+    
+})
+ScrollReveal().reveal('.review', {
+    duration:700
+    
+})
+ScrollReveal().reveal('.time', {
+    duration:700
+   
+})
+ScrollReveal().reveal('.newsletter', {
+    duration:700
+    
+})
